49cc7ff6ad6cbe3cfa708980cbf4c3e0d197e7b4
Jaimem23 <140969227+Jaimem23@users.noreply.github.com>
2026-02-14 17:17:16 +0100

    Rebuild the out page

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+   images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
